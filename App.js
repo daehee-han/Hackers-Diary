9648e9b7c7cbe52bf3b4b9567eccf567d0204104
Jangtaejin <jtjisgod@gmail.com>
2018-12-13 20:35:13 +0900

    What

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
 import main from './pages/main'
 import detail from './pages/detail'
 
@@ -20,7 +20,7 @@ export default class App extends Component {
 
     render() {
         return (
-            <View>
+            <View style={styles.view}>
                 <View style={styles.topMenu}>
                     <View style={styles.topButton}></View>
                     <View style={styles.logo}>
@@ -29,7 +29,17 @@ export default class App extends Component {
                     <View style={styles.topButton}></View>
                 </View>
                 <View style={styles.contents}>
-                    <this.state.screen></this.state.screen>
+                    <ScrollView style={{height:Dimensions.get('window').height-104}}>
+                        <this.state.screen></this.state.screen>
+                        <View style={styles.footer}>
+                            <View>
+                                <Text style={{color : '#ffffff'}}>Copyright (c) Hacker's Diary & Blogger</Text>
+                            </View>
+                            <View>
+                                <Text style={{color : '#ffffff'}}>Thanks to kjkwak12@gmail.com</Text>
+                            </View>
+                        </View>
+                    </ScrollView>
                 </View>
             </View>
         );
@@ -37,6 +47,9 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
+    view : {
+        backgroundColor : '#f2f2f2',
+    },
     topMenu : {
         flexDirection : 'row',
         justifyContent: 'space-between',
@@ -58,5 +71,11 @@ const styles = StyleSheet.create({
     logoText : {
         fontSize : 20,
         color : '#fff',
+    },
+    footer : {
+        justifyContent : "center",
+        padding : 10,
+        marginTop : 10,
+        backgroundColor : '#313131',
     }
 });
--- pages/main.js
+++ pages/main.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Dimensions } from 'react-native';
 import Axios from 'axios';
-var parseString = require('react-native-xml2js').parseString;
+const parseString = require('react-native-xml2js').parseString;
 
 export default class App extends Component {
     constructor(props) {
         super(props);
-        
         this.state = {
             feeds : []
         }
-
         this.getFeeds();
     }
     getFeeds() {
         Axios.get("http://tribal1012.tistory.com/rss")
         .then(res => {
+            // alert(res.data);
             parseString(res.data, (err, result) => {
                 feeds = [];
                 items = result.rss.channel[0].item;
@@ -23,7 +22,7 @@ export default class App extends Component {
                     feeds.push({
                         title: item.title,
                         link: item.link,
-                        description: item.description,
+                        description: item.description[0].replace(/<br\s*\/>/ig, " ").replace(/(<([^>]+)>)/ig,"").replace(/&nbsp;/ig, " ").slice(0, 200) + " ...",
                         category: item.category,
                         author: item.author,
                         guid: item.guid,
@@ -41,13 +40,15 @@ export default class App extends Component {
 
         this.state.feeds.forEach(feed => {
             feeds.push((
-                <View style={styles.feed}>
-                    <Text>{feed.title}</Text>
-                    <Text>{feed.link}</Text>
+                <View style={styles.card}>
+                    <Text style={styles.title}>{feed.title}</Text>
+                    <Text style={[styles.url, styles.introText]}>{feed.link}</Text>
+                    <Text>
+                        {feed.description}
+                    </Text>
                     <View style={styles.cardIntro}>
-                        <Text>{feed.author}</Text>
-                        <Text>{feed.category}</Text>
-                        <Text>{feed.pubDate}</Text>
+                        <Text style={[styles.author, styles.introText]}>{feed.author} > {feed.category}</Text>
+                        <Text style={[styles.pubDate, styles.introText]}>{feed.pubDate}</Text>
                     </View>
                 </View>
             ));
@@ -55,8 +56,8 @@ export default class App extends Component {
 
         return (
             <React.Fragment>
-                <Text style={styles.h1}>Main Page</Text>
-                <View>
+                <Text style={styles.h1}># Recent Feeds</Text>
+                <View style={styles.body}>
                     {feeds}
                 </View>
             </React.Fragment>
@@ -66,12 +67,24 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
     h1 : {
-        fontSize : 20,
+        fontSize : 15,
     },
-    feed : {
+    card : {
         padding : 10,
+        backgroundColor : '#fff',
+        marginTop : 10,
+    },
+    title : {
+        fontSize : 17
+    },
+    url : {
     },
     cardIntro : {
         flexDirection : 'row',
+        justifyContent: 'space-between',
     },
+    introText : {
+        fontSize : 10,
+        color : '#aaa',
+    }
 });
